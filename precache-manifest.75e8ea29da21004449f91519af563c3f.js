92d11d86464786752e77620d586da9db0b8ddf55
Vita Mileva <milayevavita27@gmail.com>
2019-08-09 08:48:40 +0300

    Updates

---
+++ precache-manifest.75e8ea29da21004449f91519af563c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613234ebd74b8c03683d1789640d0e99",
+    "url": "/cactus/index.html"
+  },
+  {
+    "revision": "7d6bde52d6a273659cd4",
+    "url": "/cactus/static/css/main.43837717.chunk.css"
+  },
+  {
+    "revision": "eaa173b7dd8ddf1e3f78",
+    "url": "/cactus/static/js/2.cce8c4e5.chunk.js"
+  },
+  {
+    "revision": "7d6bde52d6a273659cd4",
+    "url": "/cactus/static/js/main.20f5ea80.chunk.js"
+  },
+  {
+    "revision": "8566d6d3e4b1f31d24fd",
+    "url": "/cactus/static/js/runtime~main.777475c3.js"
+  },
+  {
+    "revision": "c0414aa2d4e4235af42b949bd9c88767",
+    "url": "/cactus/static/media/bg1.c0414aa2.jpg"
+  },
+  {
+    "revision": "f126e3d2a5735d21ff073f3e68f4e293",
+    "url": "/cactus/static/media/it.f126e3d2.jpg"
+  }
+]);
